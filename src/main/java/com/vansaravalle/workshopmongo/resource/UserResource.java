5c2ac941eb5a1799aebf45ac7cf461ab33431b7f
vanusasaravalle <vanusa.sousasaravalle@gmail.com>
2020-09-09 14:13:14 -0300

    Insert

--- src/main/java/com/vansaravalle/workshopmongo/resource/UserResource.java
+++ src/main/java/com/vansaravalle/workshopmongo/resource/UserResource.java
@@ -5,11 +5,10 @@ import com.vansaravalle.workshopmongo.dto.UserDTO;
 import com.vansaravalle.workshopmongo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -36,6 +35,15 @@ public class UserResource {
 
        Optional<User> user = userService.findById(id);
        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 
 
--- src/main/java/com/vansaravalle/workshopmongo/service/UserService.java
+++ src/main/java/com/vansaravalle/workshopmongo/service/UserService.java
@@ -2,6 +2,7 @@ package com.vansaravalle.workshopmongo.service;
 
 
 import com.vansaravalle.workshopmongo.domain.User;
+import com.vansaravalle.workshopmongo.dto.UserDTO;
 import com.vansaravalle.workshopmongo.repository.UserRepository;
 import com.vansaravalle.workshopmongo.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,14 @@ public class UserService {
         }
         return user;
 
+    }
+
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
